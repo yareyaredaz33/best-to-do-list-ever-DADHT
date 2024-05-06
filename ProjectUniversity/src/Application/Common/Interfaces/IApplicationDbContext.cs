3ee2ba3a1e11400e2a23beec0b62a61e1b797978
halya-h <haplykhalya@gmail.com>
2024-05-06 20:32:46 +0300

    created folders and exceptions

---
+++ ProjectUniversity/src/Application/Common/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using ProjectUniversity.Domain.Entities;
+
+namespace ProjectUniversity.Application.Common.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<TodoList> TodoLists { get; }
+
+    DbSet<TodoItem> TodoItems { get; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
