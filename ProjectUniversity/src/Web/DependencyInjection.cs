c6242ad290c729f755f74778a6cb6219cbbce133
yareyaredaz33 <52637771+yareyaredaz33@users.noreply.github.com>
2024-05-09 15:46:19 +0300

    Added method for integrating Azure Key Vault if configured

--- ProjectUniversity/src/Web/DependencyInjection.cs
+++ ProjectUniversity/src/Web/DependencyInjection.cs
@@ -44,13 +44,27 @@ public static class DependencyInjection
             configure.Title = "ProjectUniversity API";
 
             // Add the fluent validations schema processor
-            var fluentValidationSchemaProcessor = 
+            var fluentValidationSchemaProcessor =
                 sp.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
+
+            // BUG: SchemaProcessors is missing in NSwag 14 (https://github.com/RicoSuter/NSwag/issues/4524#issuecomment-1811897079)
+            // configure.SchemaProcessors.Add(fluentValidationSchemaProcessor);
 
         });
 
         return services;
     }
 
-   
+    public static IServiceCollection AddKeyVaultIfConfigured(this IServiceCollection services, ConfigurationManager configuration)
+    {
+        var keyVaultUri = configuration["KeyVaultUri"];
+        if (!string.IsNullOrWhiteSpace(keyVaultUri))
+        {
+            configuration.AddAzureKeyVault(
+                new Uri(keyVaultUri),
+                new DefaultAzureCredential());
+        }
+
+        return services;
+    }
 }
