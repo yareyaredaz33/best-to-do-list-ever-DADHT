727889a987e555bf8627e6689f1c63df6a996986
yareyaredaz33 <52637771+yareyaredaz33@users.noreply.github.com>
2024-05-09 15:45:31 +0300

    Updated dependency injection setup for web services

---
+++ ProjectUniversity/src/Web/DependencyInjection.cs
@@ -0,0 +1,56 @@
+﻿using Azure.Identity;
+using ProjectUniversity.Application.Common.Interfaces;
+using ProjectUniversity.Infrastructure.Data;
+using ProjectUniversity.Web.Services;
+using Microsoft.AspNetCore.Mvc;
+
+using ZymLabs.NSwag.FluentValidation;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+public static class DependencyInjection
+{
+    public static IServiceCollection AddWebServices(this IServiceCollection services)
+    {
+        services.AddDatabaseDeveloperPageExceptionFilter();
+
+        services.AddScoped<IUser, CurrentUser>();
+
+        services.AddHttpContextAccessor();
+
+        services.AddHealthChecks()
+            .AddDbContextCheck<ApplicationDbContext>();
+
+        services.AddExceptionHandler<CustomExceptionHandler>();
+
+        services.AddRazorPages();
+
+        services.AddScoped(provider =>
+        {
+            var validationRules = provider.GetService<IEnumerable<FluentValidationRule>>();
+            var loggerFactory = provider.GetService<ILoggerFactory>();
+
+            return new FluentValidationSchemaProcessor(provider, validationRules, loggerFactory);
+        });
+
+        // Customise default API behaviour
+        services.Configure<ApiBehaviorOptions>(options =>
+            options.SuppressModelStateInvalidFilter = true);
+
+        services.AddEndpointsApiExplorer();
+
+        services.AddOpenApiDocument((configure, sp) =>
+        {
+            configure.Title = "ProjectUniversity API";
+
+            // Add the fluent validations schema processor
+            var fluentValidationSchemaProcessor = 
+                sp.CreateScope().ServiceProvider.GetRequiredService<FluentValidationSchemaProcessor>();
+
+        });
+
+        return services;
+    }
+
+   
+}
---
+++ ProjectUniversity/src/Web/GlobalUsings.cs
@@ -0,0 +1,3 @@
+global using Ardalis.GuardClauses;
+global using ProjectUniversity.Web.Infrastructure;
+global using MediatR;
