a87de887966de39a59b5f6b090f505ab744d7fe3
halya-h <haplykhalya@gmail.com>
2024-05-09 15:39:46 +0300

    added queries

---
+++ ProjectUniversity/src/Application/ToDoLists/Queries/getTodos/TodosVm.cs
@@ -0,0 +1,10 @@
+﻿using ProjectUniversity.Application.Common.Models;
+
+namespace ProjectUniversity.Application.TodoLists.Queries.GetTodos;
+
+public class TodosVm
+{
+    public IReadOnlyCollection<LookupDto> PriorityLevels { get; init; } = Array.Empty<LookupDto>();
+
+    public IReadOnlyCollection<TodoListDto> Lists { get; init; } = Array.Empty<TodoListDto>();
+}
